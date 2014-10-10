b4fc5ec694e522b7261e7ac91dc4637357fae956
Goran Mandinić <goran.mandinic@gmail.com>
2014-10-10 20:59:05 +0200

    Delete 20140917092111_add_checked_num_to_cart.rb

--- db/migrate/20140917092111_add_checked_num_to_cart.rb
+++
@@ -1,9 +0,0 @@
-class AddCheckedNumToCart < ActiveRecord::Migration
-  def self.up
-    add_column :carts, :checked_num, :integer
-  end
-  def self.down
-    remove_column :carts, :checked_num
-  end
-  
-end
