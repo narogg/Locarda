aa3fb4f8367b34829f33ec7dcbb18835a83e9d03
Goran Mandinić <goran.mandinic@gmail.com>
2014-10-10 20:59:20 +0200

    Delete 20140919120145_add_admin_id_to_places.rb

--- db/migrate/20140919120145_add_admin_id_to_places.rb
+++
@@ -1,9 +0,0 @@
-class AddAdminIdToPlaces < ActiveRecord::Migration
-  def self.up
-    add_column :places, :admin_id, :integer
-  end
-  
-  def self.down
-    remove_column :places, :admin_id
-  end
-end
